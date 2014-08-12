cb58eded30667f3fb39c352babdfd219180223a3
Juan Camilo <xogost@gmail.com>
2014-08-12 15:08:11 -0500

    Se corrige el cambio en base de datos para la tabla t14 para tipo de persona juridica y nombre de persona juridica

--- application/third_party/Se cambia el tipo de dato de el campo tipo de persona juridica en la tabla t14.sql
+++ application/third_party/Se cambia el tipo de dato de el campo tipo de persona juridica en la tabla t14.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `quimbo`.`t14web_certificaciones_detalle`   
-  CHANGE `a14NombrePersonaJuridica` `a14NombrePersonaJuridica` SMALLINT(2) NULL  COMMENT 'Nombre completo de la persona jurídica que certifica';
+  CHANGE `a14TipoPersonaJuridica` `a14TipoPersonaJuridica` SMALLINT(2) NULL  COMMENT 'Junta de acción comunal, Unidad de justicia y comisaría de familia, Personería, Alcaldía, otro',
+  CHANGE `a14NombrePersonaJuridica` `a14NombrePersonaJuridica` VARCHAR(500) NULL  COMMENT 'Nombre completo de la persona jurídica que certifica';
