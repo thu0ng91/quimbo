fa9b683faf3842dcc167d092e3d43163aa3d4fbe
Juan Camilo <xogost@gmail.com>
2014-08-12 13:19:59 -0500

    Se agrega script para cambiar tipo de dato de persona juridica en la tabla t14

---
+++ application/third_party/Se cambia el tipo de dato de el campo tipo de persona juridica en la tabla t14.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `quimbo`.`t14web_certificaciones_detalle`   
+  CHANGE `a14NombrePersonaJuridica` `a14NombrePersonaJuridica` SMALLINT(2) NULL  COMMENT 'Nombre completo de la persona jurídica que certifica';
