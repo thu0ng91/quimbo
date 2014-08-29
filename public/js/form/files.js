f64366249829d52ec2d30aec4a5caf5e8eb49b85
MadkaT182 <madcat182@gmail.com>
2014-08-29 18:26:10 -0500

    Se agregan nomenclaturas para el tipo de archivo

--- public/js/form/files.js
+++ public/js/form/files.js
@@ -4,26 +4,85 @@ loadControlValues();
 $("#txtIdentificador").html(getParameterByName("formCode"));
 
 function loadControlValues(){
-	var code = getParameterByName("formCode");
-	var tabla = "";
-	$(".modal").modal('show');
-	$.getJSON("index.php/form/get_FilesN/" + code, function(objRData){
-		arrayNDocuments = objRData;
+    var code = getParameterByName("formCode");
+    var tabla = "";
+    $(".modal").modal('show');
+    $.getJSON("index.php/form/get_FilesN/" + code, function(objRData){
+        arrayNDocuments = objRData;
 
-		if (arrayNDocuments.length >= 0){
-			tabla += "<table border='1' cellpadding='1' cellspacing='1' style='width: 65%;'><thead><tr><th scope='col'>Tipo de Documento</th><th scope='col'>Detalle</th></tr></thead><tbody>";
+        if (arrayNDocuments.length >= 0){
+            tabla += "<table border='1' cellpadding='1' cellspacing='1' style='width: 65%;'><thead><tr><th scope='col'>Tipo de Documento</th><th scope='col'>Detalle</th></tr></thead><tbody>";
 
-			for (var i = arrayNDocuments.length - 1; i >= 0; i--) {
-				var ruta = arrayNDocuments[i].a18RutaArchivo.replace("amazon_root", "https://s3.amazonaws.com");
-				tabla += "<tr><td>" + arrayNDocuments[i].a18TipoArchivo + "</td><td>" + "<a href='" + ruta + "' class='btn btn-success'>Ver Detalle</a>" + "</td></tr>";
-			}
+            for (var i = arrayNDocuments.length - 1; i >= 0; i--) {
+                var ruta = arrayNDocuments[i].a18RutaArchivo.replace("amazon_root", "https://s3.amazonaws.com");
+                var tipo = "";
 
-			tabla += "</tbody></table><br/>";
-			$("#tableResults").html(tabla);
-		}
+                switch(arrayNDocuments[i].a18TipoArchivo) {
+                case "01Formato":
+                    tipo = "Formato de inscripción y Habeas Data";
+                    break;
 
-	$(".modal").modal('hide');
-	});
+                case "02Cedula":
+                    tipo = "Cédula de ciudadanía";
+                    break;
+
+                case "03Poder":
+                    tipo = "Poder";
+                    break;
+
+                case "04CL":
+                    tipo = "Certificación Laboral";
+                    break;
+
+                case "05AutoEntidades":
+                    tipo = "Auto Entidades";
+                    break;
+
+                case "06Terceros":
+                    tipo = "Terceros";
+                    break;
+
+                case "07CertComercial":
+                    tipo = "Certificación Comercial";
+                    break;
+
+                case "08Factura":
+                    tipo = "Factura";
+                    break;
+
+                case "09CertVecindad":
+                    tipo = "Certificación de Vecindad";
+                    break;
+                case "10DerPeticion":
+                    tipo = "Derecho de petición";
+                    break;
+
+                case "11RadicadosEmgesa":
+                    tipo = "Radicados Emgesa";
+                    break;
+
+                case "12Otros":
+                    tipo = "Otros Documentos";
+                    break;
+
+                case "Formato No VÃ¡lido":
+                    tipo = "Formato No Válido";
+                    break;
+                    
+                case "Video":
+                    tipo = "Video";
+                    break;
+                }
+
+                tabla += "<tr><td>" + tipo + "</td><td>" + "<a href='" + ruta + "' class='btn btn-success'>Ver Detalle</a>" + "</td></tr>";
+            }
+
+            tabla += "</tbody></table><br/>";
+            $("#tableResults").html(tabla);
+        }
+
+    $(".modal").modal('hide');
+    });
 }
 
 //-Extraer parametros QueryString-//
